b687d499a8c753c742d068f3796cb0d81cace1e7
travp624 <stygianstudiosdevs@gmail.com>
2012-04-21 08:22:19 -0500

    fix navbar bg color chooser crashing systemui on devices without a navbar by default.
    Signed-off-by: travp624 <stygianstudiosdevs@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2715,20 +2715,26 @@ public class PhoneStatusBar extends StatusBar {
             mDateView.setOnTouchListener(null);
         }
 
-        // NavigationBar background color
-        final int DEFAULT_BACKGROUND_COLOR = 0xFF000000;
-        int navbarBackgroundColor = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.NAVIGATION_BAR_BACKGROUND_COLOR, DEFAULT_BACKGROUND_COLOR);
-        if (DEBUG) {
-                if (DEFAULT_BACKGROUND_COLOR != navbarBackgroundColor) Log.d(TAG, String.format
+        try {
+            boolean showNav = mWindowManager.hasNavigationBar();
+            if (showNav) {
+                // NavigationBar background color
+                final int DEFAULT_BACKGROUND_COLOR = 0xFF000000;
+                int navbarBackgroundColor = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_BACKGROUND_COLOR, DEFAULT_BACKGROUND_COLOR);
+                if (DEBUG) {
+                    if (DEFAULT_BACKGROUND_COLOR != navbarBackgroundColor) Log.d(TAG, String.format
                         ("background navbar color found to be: %d", navbarBackgroundColor));
-                else Log.d(TAG, "default navbar color found");
+                    else Log.d(TAG, "default navbar color found");
+                }
+                if (navbarBackgroundColor != DEFAULT_BACKGROUND_COLOR)
+                    mNavigationBarView.setBackgroundColor(navbarBackgroundColor);
+                else
+                    mNavigationBarView.setBackgroundColor(DEFAULT_BACKGROUND_COLOR);
+            }
+        } catch (RemoteException ex) {
+            // no window manager? good luck with that
         }
-        if (navbarBackgroundColor != DEFAULT_BACKGROUND_COLOR)
-            mNavigationBarView.setBackgroundColor(navbarBackgroundColor);
-        else
-            mNavigationBarView.setBackgroundColor(DEFAULT_BACKGROUND_COLOR);
-
        reDrawHeader();
 
     }
