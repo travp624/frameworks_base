aa2c20dcc93f887f4a2071288e3843bd9cf4e0e1
travp624 <stygianstudiosdevs@gmail.com>
2012-05-14 19:54:53 -0500

    Add this to make fm radio work on 2nd-gen moto devices [x13thangelx]

--- core/java/android/server/BluetoothAdapterStateMachine.java
+++ core/java/android/server/BluetoothAdapterStateMachine.java
@@ -387,7 +387,10 @@ final class BluetoothAdapterStateMachine extends StateMachine {
                 case USER_TURN_OFF: // ignore
                     break;
                 case POWER_STATE_CHANGED:
-                    if ((Boolean) message.obj) {
+                    if (message.what == 54) {
+                        log("Moto FM Radio");
+                    }
+                    else if ((Boolean) message.obj) {
                         recoverStateMachine(TURN_HOT, null);
                     }
                     break;
