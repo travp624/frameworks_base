919f2c8fdf9136f81e5e60be40917fdaa3af3788
travp624 <travp624@gmail.com>
2012-06-02 13:25:47 -0500

    SystemUI Notification bar counter.
    Credit to Cyanogen.
    
    Displays number of pending notifications in the status bar.
    
    I changed the ugly red to ics blue. I think it looks awesome.
    
    Change-Id: I656ee96a9960f5ed13888a155a5aa95136dbbbf8

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -56,6 +56,7 @@ public class StatusBarIconView extends AnimatedImageView {
     private Notification mNotification;
 
     private Handler mHandler;
+    private boolean mShowNotificationCount;
 
     private static final Mode SCREEN_MODE = Mode.MULTIPLY;
 
@@ -64,13 +65,21 @@ public class StatusBarIconView extends AnimatedImageView {
     public StatusBarIconView(Context context, String slot, Notification notification) {
         super(context);
         final Resources res = context.getResources();
+        final float densityMultiplier = res.getDisplayMetrics().density;
+        final float scaledPx = 10 * densityMultiplier;
         mSlot = slot;
         mNumberPain = new Paint();
         mNumberPain.setTextAlign(Paint.Align.CENTER);
         mNumberPain.setColor(res.getColor(R.drawable.notification_number_text_color));
         mNumberPain.setAntiAlias(true);
+        mNumberPain.setTextSize(scaledPx);
         mNotification = notification;
+        mShowNotificationCount = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_NOTIF_COUNT, 0) == 1;
         setContentDescription(notification);
+
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe();
 
         // We do not resize and scale system icons (on the right), only notification icons (on the
         // left).
@@ -125,6 +134,10 @@ public class StatusBarIconView extends AnimatedImageView {
      * Returns whether the set succeeded.
      */
     public boolean set(StatusBarIcon icon) {
+        return set(icon, false);
+    }
+
+    private boolean set(StatusBarIcon icon, boolean force) {
         final boolean iconEquals = mIcon != null
                 && streq(mIcon.iconPackage, icon.iconPackage)
                 && mIcon.iconId == icon.iconId;
@@ -136,7 +149,7 @@ public class StatusBarIconView extends AnimatedImageView {
                 && mIcon.number == icon.number;
         mIcon = icon.clone();
         setContentDescription(icon.contentDescription);
-        if (!iconEquals) {
+        if (!iconEquals || force) {
             Drawable drawable = getIcon(icon);
             if (drawable == null) {
                 Slog.w(StatusBar.TAG, "No icon for slot " + mSlot);
@@ -144,13 +157,12 @@ public class StatusBarIconView extends AnimatedImageView {
             }
             setImageDrawable(drawable);
         }
-        if (!levelEquals) {
+        if (!levelEquals || force) {
             setImageLevel(icon.iconLevel);
         }
 
-        if (!numberEquals) {
-            if (icon.number > 0 && mContext.getResources().getBoolean(
-                        R.bool.config_statusBarShowNumber)) {
+        if (!numberEquals || force) {
+            if (icon.number > 0 && mShowNotificationCount) {
                 if (mNumberBackground == null) {
                     mNumberBackground = getContext().getResources().getDrawable(
                             R.drawable.ic_notification_overlay);
@@ -162,7 +174,7 @@ public class StatusBarIconView extends AnimatedImageView {
             }
             invalidate();
         }
-        if (!visibilityEquals) {
+        if (!visibilityEquals || force) {
             setVisibility(icon.visible ? VISIBLE : GONE);
         }
         return true;
@@ -314,6 +326,10 @@ public class StatusBarIconView extends AnimatedImageView {
 
         @Override
         public void onChange(boolean selfChange) {
+            mShowNotificationCount = Settings.System.getInt(
+                    mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_NOTIF_COUNT, 0) == 1;
+            set(mIcon, true);
             updateSettings();
         }
     }    
